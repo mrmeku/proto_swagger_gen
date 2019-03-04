287216f1169f41571bb5b1433372e3c556881a2c
Dan Muller <mrmeku@gmail.com>
2019-03-04 00:16:56 -0700

    More reliable path for copying artifacts

--- proto_swagger_gen.sh
+++ proto_swagger_gen.sh
@@ -1,13 +1,18 @@
-docker run -m 8GB --cpus 8 -e USER="$(id -u)" \
+docker run -m 4GB --cpus 4 -e USER="$(id -u)" \
  -u="$(id -u)" \
- -v ~/proto_swagger_gen:/src/workspace \
- -v ~/proto_swagger_gen/dist:/tmp/build_output \
+ -v $(pwd):/src/workspace \
+ -v $(pwd)/docker_cache:/tmp \
  -w /src/workspace \
  l.gcr.io/google/bazel:latest \
  --output_user_root=/tmp/build_output \
- build //api/... --symlink_prefix=dist/
+ --output_user_root=/tmp/execroot \
+ --output_user_root=/tmp/cache \
+ --output_user_root=/tmp/install \
+ --output_user_root=/tmp/external \
+ --output_user_root=/tmp/server \
+ --output_base=/tmp build //api/...
 
 rm -rf proto_swagger_gen_out
 mkdir proto_swagger_gen_out
 
-cp --no-preserve=mode,ownership -rLf dist/a08c2e4811c846650b733c6fc815a920/execroot/proto_swagger_gen/bazel-out/k8-fastbuild/bin/api/* proto_swagger_gen_out
+cp --no-preserve=mode,ownership -rLf docker_cache/execroot/proto_swagger_gen/bazel-out/k8-fastbuild/bin/api/* proto_swagger_gen_out
